f890b8cd2079e961dad27d73abe344541adea751
shidemere <tihopadaetnsneg@gmail.com>
2025-12-19 19:02:48 +0300

    third solved

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"hexlet-go/greeting"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Println(greeting.Hello())
+	//fmt.Println(greeting.Hello())
+	color.Red(greeting.Hello())
+	color.Blue(greeting.Hello())
+	color.Cyan(greeting.Hello())
 }
